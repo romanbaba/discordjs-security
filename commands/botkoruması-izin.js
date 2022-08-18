b690ea2c8ffe0941e8682211fc8108896ecc9f31
Roman <108236042+romanwashere@users.noreply.github.com>
2022-08-18 14:26:58 +0300

    Update botkoruması-izin.js

--- commands/botkoruması-izin.js
+++ commands/botkoruması-izin.js
@@ -1,4 +1,4 @@
-const { MessageEmbed } = require(`discord.js`);
+const { EmbedBuilder } = require(`discord.js`);
 
 module.exports = {
 	name: `botkoruması-izin`,
@@ -8,7 +8,7 @@ module.exports = {
 
 		const { author, guild, channel } = message;
     if(!message.member.permissions.has('ADMINISTRATOR')) {
-              return message.reply({ embeds: [ new MessageEmbed().setDescription('<:cross:996678479363969075> | Bu komutunu kullanabilmek için `Yönetici` iznine sahip olman gerekiyor. ').setColor('RED') ] });
+              return message.reply({ embeds: [ new EmbedBuilder().setDescription('<:cross:996678479363969075> | Bu komutunu kullanabilmek için `Yönetici` iznine sahip olman gerekiyor. ').setColor('RED') ] });
             }
 		const database = db.fetch(`logChannel_${guild.id}`)
 		const botDatabase = db.fetch(`botDatabase_${guild.id}`)
@@ -16,17 +16,17 @@ module.exports = {
 		const botIdDatabase = db.fetch(`botIdDatabase_${id}${guild.id}`)
 
 
-		if(!database) return message.reply({ embeds: [new MessageEmbed().setDescription('<:cross:996678479363969075> | Bu komutu kullanmak için log kanalını ayarlaman gerekiyor.').setColor('RED') ]})	
-		if(!botDatabase) return message.reply({ embeds: [new MessageEmbed().setDescription('<:cross:996678479363969075> | Bu komutu kullanmak için bot koruma komutunu aktif etmen gerekiyor.').setColor('RED') ]})		
-		if(!id) return message.reply({ embeds: [new MessageEmbed().setDescription('<:cross:996678479363969075> | Bir ID girmen gerekiyor.').setColor('RED') ]})					
+		if(!database) return message.reply({ embeds: [new EmbedBuilder().setDescription('<:cross:996678479363969075> | Bu komutu kullanmak için log kanalını ayarlaman gerekiyor.').setColor('RED') ]})	
+		if(!botDatabase) return message.reply({ embeds: [new EmbedBuilder().setDescription('<:cross:996678479363969075> | Bu komutu kullanmak için bot koruma komutunu aktif etmen gerekiyor.').setColor('RED') ]})		
+		if(!id) return message.reply({ embeds: [new EmbedBuilder().setDescription('<:cross:996678479363969075> | Bir ID girmen gerekiyor.').setColor('RED') ]})					
 
 		if(database) {
 
 		if(botDatabase) {
 
 
-		if(botIdDatabase) return message.reply({ embeds: [new MessageEmbed().setDescription('<:cross:996678479363969075> | Bu bot zaten beyaz listeye eklenmiş.').setColor('RED') ]})						
-		if(isNaN(id)) return message.reply({ embeds: [new MessageEmbed().setDescription('<:cross:996678479363969075> | Bir ID girmen gerekiyor.').setColor('RED') ]})					
+		if(botIdDatabase) return message.reply({ embeds: [new EmbedBuilder().setDescription('<:cross:996678479363969075> | Bu bot zaten beyaz listeye eklenmiş.').setColor('RED') ]})						
+		if(isNaN(id)) return message.reply({ embeds: [new EmbedBuilder().setDescription('<:cross:996678479363969075> | Bir ID girmen gerekiyor.').setColor('RED') ]})					
 		if(id.length === 18) {
 
 			db.set(`botIdDatabase_${id}${guild.id}`, {
@@ -36,12 +36,12 @@ module.exports = {
 		})
 		message.reply({
 			embeds: [
-			new MessageEmbed().setDescription(`<:check:996678482266431580> | **${id}** Beyaz listeye eklendi.`).setColor("GREEN").setFooter({ text:  `${author.tag} tarafından istendi.`, iconURL: author.displayAvatarURL({ dynamic: true })}).setTimestamp()
+			new EmbedBuilder().setDescription(`<:check:996678482266431580> | **${id}** Beyaz listeye eklendi.`).setColor("GREEN").setFooter({ text:  `${author.tag} tarafından istendi.`, iconURL: author.displayAvatarURL({ dynamic: true })}).setTimestamp()
 			]
 		})
 
 		} else {
-			return message.reply({ embeds: [new MessageEmbed().setDescription('<:cross:996678479363969075> | Bir ID girmen gerekiyor.').setColor('RED') ]})						
+			return message.reply({ embeds: [new EmbedBuilder().setDescription('<:cross:996678479363969075> | Bir ID girmen gerekiyor.').setColor('RED') ]})						
 		}
 
 			} 				
