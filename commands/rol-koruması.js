f3c5c98cab237b925f8cf6be7602ccb6d5a7c9fa
Roman <108236042+romanwashere@users.noreply.github.com>
2022-08-18 14:15:38 +0300

    Update rol-koruması.js

--- commands/rol-koruması.js
+++ commands/rol-koruması.js
@@ -1,4 +1,4 @@
-const { MessageEmbed } = require(`discord.js`);
+const { EmbedBuilder } = require(`discord.js`);
 
 module.exports = {
 	name: `rol-koruması`,
@@ -8,21 +8,21 @@ module.exports = {
 
 		const { author, guild, channel } = message;
     if(!message.member.permissions.has('MANAGE_ROLES')) {
-              return message.reply({ embeds: [ new MessageEmbed().setDescription('<:cross:996678479363969075> | Bu komutunu kullanabilmek için `Rolleri Yönet` iznine sahip olman gerekiyor. ').setColor('RED') ]});
+              return message.reply({ embeds: [ new EmbedBuilder().setDescription('<:cross:996678479363969075> | Bu komutunu kullanabilmek için `Rolleri Yönet` iznine sahip olman gerekiyor. ').setColor('Red') ]});
             }
 		const database = db.fetch(`logChannel_${guild.id}`)
 		const roleDatabase = db.fetch(`guardRole_${guild.id}`)
 
-		if(!database) return message.reply({ embeds: [new MessageEmbed().setDescription('<:cross:996678479363969075> | Bu komutu kullanmak için log komutunu aktif edin.').setColor('RED') ]})	
+		if(!database) return message.reply({ embeds: [new EmbedBuilder().setDescription('<:cross:996678479363969075> | Bu komutu kullanmak için log komutunu aktif edin.').setColor('Red') ]})	
 		
 		if(database) {
 
-			if(roleDatabase) return message.reply({ embeds: [new MessageEmbed().setDescription(`<:cross:996678479363969075> | Rol koruması zaten aktif edilmiş.`)] })			
+			if(roleDatabase) return message.reply({ embeds: [new EmbedBuilder().setDescription(`<:cross:996678479363969075> | Rol koruması zaten aktif edilmiş.`)] })			
 
 			if(!roleDatabase) {
 
 				db.set(`guardRole_${guild.id}`, true)
-				message.reply({ embeds: [new MessageEmbed().setDescription('<:check:996678482266431580> | Rol koruması **aktif** edildi!').setColor('GREEN').setFooter({ text:  `${author.tag} tarafından istendi.`, iconURL: author.displayAvatarURL({ dynamic: true })}).setTimestamp()] })
+				message.reply({ embeds: [new EmbedBuilder().setDescription('<:check:996678482266431580> | Rol koruması **aktif** edildi!').setColor('Green').setFooter({ text:  `${author.tag} tarafından istendi.`, iconURL: author.displayAvatarURL({ dynamic: true })}).setTimestamp()] })
 
 			}				
 
