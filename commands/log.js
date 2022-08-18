2838c74b9f49f504a7c135cb44f78ea46115ae65
Roman <108236042+romanwashere@users.noreply.github.com>
2022-08-18 14:19:57 +0300

    Update log.js

--- commands/log.js
+++ commands/log.js
@@ -1,4 +1,4 @@
-const { MessageEmbed } = require(`discord.js`);
+const { EmbedBuilder } = require(`discord.js`);
 
 module.exports = {
 	name: `log`,
@@ -8,16 +8,16 @@ module.exports = {
 
 		const { author, guild, channel } = message;
     if(!message.member.permissions.has('ADMINISTRATOR')) {
-              return message.reply({ embeds: [ new MessageEmbed().setDescription('<:cross:996678479363969075> | Bu komutunu kullanabilmek için `Yönetici` iznine sahip olman gerekiyor. ').setColor('RED') ] });
+              return message.reply({ embeds: [ new EmbedBuilder().setDescription('<:cross:996678479363969075> | Bu komutunu kullanabilmek için `Yönetici` iznine sahip olman gerekiyor. ').setColor('RED') ] });
             }
 
 		const kanal = message.mentions.channels.first()
-		if(!kanal) return message.reply({ embeds: [new MessageEmbed().setDescription('<:cross:996678479363969075> | Bir kanal etiketlemen gerekiyor.').setColor('RED') ]})
+		if(!kanal) return message.reply({ embeds: [new EmbedBuilder().setDescription('<:cross:996678479363969075> | Bir kanal etiketlemen gerekiyor.').setColor('RED') ]})
 		
 		if(kanal) {
 
 			const database = db.fetch(`logChannel_${guild.id}`)
-			if(database) return message.reply({ embeds: [new MessageEmbed().setDescription('<:cross:996678479363969075> | Log kanalı zaten aktif edilmiş.').setColor('RED') ]})	
+			if(database) return message.reply({ embeds: [new EmbedBuilder().setDescription('<:cross:996678479363969075> | Log kanalı zaten aktif edilmiş.').setColor('RED') ]})	
 
 			if(!database) {
 
@@ -25,7 +25,7 @@ module.exports = {
 					channel: kanal.id, 
 					author: author.id 
 				})
-				message.reply({ embeds: [new MessageEmbed().setDescription('<:check:996678482266431580> | Log kanalı **aktif** edildi!').setColor('GREEN').setFooter({ text:  `${author.tag} tarafından istendi.`, iconURL: author.displayAvatarURL({ dynamic: true })}).setTimestamp()]})
+				message.reply({ embeds: [new EmbedBuilder().setDescription('<:check:996678482266431580> | Log kanalı **aktif** edildi!').setColor('GREEN').setFooter({ text:  `${author.tag} tarafından istendi.`, iconURL: author.displayAvatarURL({ dynamic: true })}).setTimestamp()]})
 
 			}	
 
