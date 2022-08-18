c39e903ebc693fdf1beaec4f88f9008b2deee86a
Roman <108236042+romanwashere@users.noreply.github.com>
2022-08-18 14:23:09 +0300

    Update link-koruması.js

--- commands/link-koruması.js
+++ commands/link-koruması.js
@@ -1,4 +1,4 @@
-const { MessageEmbed } = require(`discord.js`);
+const { EmbedBuilder } = require(`discord.js`);
 
 module.exports = {
 	name: `link-koruması`,
@@ -8,19 +8,19 @@ module.exports = {
 
 		const { author, guild, channel } = message;
     if(!message.member.permissions.has('MANAGE_MESSAGES')) {
-              return message.reply({ embeds: [ new MessageEmbed().setDescription('<:cross:996678479363969075> | Bu komutunu kullanabilmek için `Mesajları Yönet` iznine sahip olman gerekiyor. ').setColor('RED') ]});
+              return message.reply({ embeds: [ new EmbedBuilder().setDescription('<:cross:996678479363969075> | Bu komutunu kullanabilmek için `Mesajları Yönet` iznine sahip olman gerekiyor. ').setColor('Red') ]});
             }
 		const database = db.fetch(`logChannel_${guild.id}`)
 		const urlDatabase = db.fetch(`urlDatabase_${guild.id}`)
 
-		if(!database) return message.reply({ embeds: [new MessageEmbed().setDescription('<:cross:996678479363969075> | Bu komutu kullanmak için log kanalını ayarlaman gerekiyor.').setColor('RED') ]})	
+		if(!database) return message.reply({ embeds: [new EmbedBuilder().setDescription('<:cross:996678479363969075> | Bu komutu kullanmak için log kanalını ayarlaman gerekiyor.').setColor('Red') ]})	
 		
 		if(database) {
 
 			db.set(`urlDatabase_${guild.id}`, true)
 			message.reply({
 				embeds: [
-				new MessageEmbed().setDescription(`<:check:996678482266431580> | Sunucuda olan linkleri **yöneticiler hariç** hepsini engelliyeceiğim!`).setColor('GREEN').setFooter({ text:  `${author.tag} tarafından istendi.`, iconURL: author.displayAvatarURL({ dynamic: true })}).setTimestamp()
+				new EmbedBuilder().setDescription(`<:check:996678482266431580> | Sunucuda olan linkleri **yöneticiler hariç** hepsini engelliyeceiğim!`).setColor('Green').setFooter({ text:  `${author.tag} tarafından istendi.`, iconURL: author.displayAvatarURL({ dynamic: true })}).setTimestamp()
 				]
 			})
 
