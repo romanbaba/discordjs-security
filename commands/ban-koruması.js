a3fb72aabbd08edcef13defaf96c2c6323d405f3
Roman <108236042+romanwashere@users.noreply.github.com>
2022-08-18 14:29:10 +0300

    Update ban-koruması.js

--- commands/ban-koruması.js
+++ commands/ban-koruması.js
@@ -1,4 +1,4 @@
-const { MessageEmbed } = require(`discord.js`);
+const { EmbedBuilder } = require(`discord.js`);
 
 module.exports = {
 	name: `ban-koruması`,
@@ -8,21 +8,21 @@ module.exports = {
 
 		const { author, guild, channel } = message;
     if(!message.member.permissions.has('ADMINISTRATOR')) {
-              return message.reply({ embeds: [ new MessageEmbed().setDescription('<:cross:996678479363969075> | Bu komutunu kullanabilmek için `Yönetici` iznine sahip olman gerekiyor. ').setColor('RED') ] });
+              return message.reply({ embeds: [ new EmbedBuilder().setDescription('<:cross:996678479363969075> | Bu komutunu kullanabilmek için `Yönetici` iznine sahip olman gerekiyor. ').setColor('Red') ] });
             }
 		const database = db.fetch(`logChannel_${guild.id}`)
 		const botDatabase = db.fetch(`guardBan_${guild.id}`)
 
-		if(!database) return message.reply({ embeds: [new MessageEmbed().setDescription('<:cross:996678479363969075> | Bu komutu kullanmak için log komutunu aktif edin.').setColor('RED') ]})	
+		if(!database) return message.reply({ embeds: [new EmbedBuilder().setDescription('<:cross:996678479363969075> | Bu komutu kullanmak için log komutunu aktif edin.').setColor('Red') ]})	
 		
 		if(database) {
 
-			if(botDatabase) return message.reply({ embeds: [new MessageEmbed().setDescription(`<:cross:996678479363969075> | Ban koruması zaten aktif edilmiş.`)] })			
+			if(botDatabase) return message.reply({ embeds: [new EmbedBuilder().setDescription(`<:cross:996678479363969075> | Ban koruması zaten aktif edilmiş.`)] })			
 
 			if(!botDatabase) {
 
 				db.set(`guardBan_${guild.id}`, true)
-				message.reply({ embeds: [new MessageEmbed().setDescription('<:check:996678482266431580> | Ban koruması **aktif** edildi!').setColor('GREEN').setFooter({ text:  `${author.tag} tarafından istendi.`, iconURL: author.displayAvatarURL({ dynamic: true })}).setTimestamp()] })
+				message.reply({ embeds: [new EmbedBuilder().setDescription('<:check:996678482266431580> | Ban koruması **aktif** edildi!').setColor('Green').setFooter({ text:  `${author.tag} tarafından istendi.`, iconURL: author.displayAvatarURL({ dynamic: true })}).setTimestamp()] })
 
 			}				
 
