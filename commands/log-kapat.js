77270ee58883ba6b4ad82a735693788635bc1af3
Roman <108236042+romanwashere@users.noreply.github.com>
2022-08-18 14:21:00 +0300

    Update log-kapat.js

--- commands/log-kapat.js
+++ commands/log-kapat.js
@@ -1,4 +1,4 @@
-const { MessageEmbed } = require(`discord.js`);
+const { EmbedBuilder } = require(`discord.js`);
 
 module.exports = {
 	name: `log-kapat`,
@@ -8,15 +8,15 @@ module.exports = {
 
 		const { author, guild, channel } = message;
     if(!message.member.permissions.has('ADMINISTRATOR')) {
-              return message.reply({ embeds: [ new MessageEmbed().setDescription('<:cross:996678479363969075> | Bu komutunu kullanabilmek için `Yönetici` iznine sahip olman gerekiyor. ').setColor('RED') ] });
+              return message.reply({ embeds: [ new EmbedBuilder().setDescription('<:cross:996678479363969075> | Bu komutunu kullanabilmek için `Yönetici` iznine sahip olman gerekiyor. ').setColor('Red') ] });
             }
 		const database = db.fetch(`logChannel_${guild.id}`)
 
-		if(!database) return message.reply({ embeds: [new MessageEmbed().setDescription('<:cross:996678479363969075> | Log komutu zaten deaktif edilmiş.').setColor('RED') ]})	
+		if(!database) return message.reply({ embeds: [new EmbedBuilder().setDescription('<:cross:996678479363969075> | Log komutu zaten deaktif edilmiş.').setColor('Red') ]})	
 		
 		if(database) {
 
-			message.reply({ embeds: [new MessageEmbed().setDescription('<:parkle:996678480764870747> | Datalar siliniyor.. bu biraz zaman alabilir.').setColor('RED')] }).then(msg => { 
+			message.reply({ embeds: [new EmbedBuilder().setDescription('<:parkle:996678480764870747> | Datalar siliniyor.. bu biraz zaman alabilir.').setColor('Red')] }).then(msg => { 
 
 			setTimeout(function() {
 
@@ -30,7 +30,7 @@ module.exports = {
 			db.delete(`accountDatabase_${guild.id}`)
 
 
-msg.edit({ embeds: [new MessageEmbed().setDescription('<:check:996678482266431580> | Log kanalı ve datalar **deaktif** edildi!').setColor('GREEN').setFooter({ text:  `${author.tag} tarafından istendi.`, iconURL: author.displayAvatarURL({ dynamic: true })}).setTimestamp()]})
+msg.edit({ embeds: [new EmbedBuilder().setDescription('<:check:996678482266431580> | Log kanalı ve datalar **deaktif** edildi!').setColor('Green').setFooter({ text:  `${author.tag} tarafından istendi.`, iconURL: author.displayAvatarURL({ dynamic: true })}).setTimestamp()]})
 
 			}, 5000);
 
